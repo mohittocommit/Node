1dc8e70e9f6b457c5299486782a22375528b2a84
Mohit Chouhan <rahul@globalcognito.com>
2024-07-12 11:01:36 +0530

    login api creation

--- bookMyShowBackend/app.js
+++ bookMyShowBackend/app.js
@@ -1,10 +1,25 @@
 const express = require("express")
+const bodyParser = require("body-parser"); // Middleware
 const app = express();
 const routes = require("./src/routes")
 
 
-app.use("/",routes)
+// Add body parser as middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+// Middleware to set CORS headers
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+
+app.use("/",routes)
 
 const port = 9001;
 app.listen(port, ()=> {
--- bookMyShowBackend/src/routes.js
+++ bookMyShowBackend/src/routes.js
@@ -9,14 +9,28 @@ router.get("/", (req, res) => {
     return res.send("Hello World!")
 });
 router.post("/api/register", async (req, res) => {
-    const { name, email, phone, password } = req.query;
+    const { name, email, phone, password } = req.body;
 
     /* password encryption */
     const salt = await bcrypt.genSaltSync(10);
     const hashPassword = await bcrypt.hashSync(password, salt);
 
-    const sqlQuery = `INSERT INTO users(name,email,phone,password) VALUES(?,?,?,?)`;
+    db.query(`SELECT * FROM users where email='${email}'`, (error, result) => {
+        if (error) {
+            return res.status(500).send(error);
+        }
+        if (result) {
+            if (result.length > 0) {
+                const response = {
+                    message: "This email alredy exist",
+                    status: false
+                }
+                return res.status(401).json(response)
+            }
+        }
+    });
 
+    const sqlQuery = `INSERT INTO users(name,email,phone,password) VALUES(?,?,?,?)`;
     db.query(sqlQuery, [name, email, phone, hashPassword], (error, result) => {
         if (error) {
             res.status(500).send(error);
@@ -28,9 +42,50 @@ router.post("/api/register", async (req, res) => {
             }
             return res.json(response)
         }
-
     })
 })
+
+router.post("/api/login", (req, res) => {
+    const { email, password } = req.body;
+
+    const sqlQuery = `SELECT * FROM users WHERE email='${email}'`;
+    db.query(sqlQuery, async (error, result) => {
+        if (error) {
+            return res.status(500).send(error);
+        }
+        if (result) {
+            if (result.length === 0) {
+                const response = {
+                    message: "Invalide login details!",
+                    status: false
+                }
+                return res.status(401).json(response);
+            }
+            const hashPassword = result[0].password
+            const match = await bcrypt.compare(password, hashPassword);
+
+            if (match) {
+                const payload = { email: email };
+                const secretKey = "webdev24";
+                const accessToken = await jwt.sign(payload, secretKey);
+                const response = {
+                    token: accessToken,
+                    message: "Login Successfully",
+                    status: true
+                }
+                return res.json(response)
+            } else {
+                const response = {
+                    message: "Invalide login details!",
+                    status: false
+                }
+                return res.status(401).json(response);
+            }
+        }
+    })
+})
+
+
 router.get("/api/users", (req, res) => {
     db.query("SELECT * FROM users", (error, result) => {
         if (error) {
